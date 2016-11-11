5bff538575d505b27e282c063d77f9269db115af
xrr2016 <13286904669@163.com>
2016-11-11 19:19:49 +0800

    finish user routes

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,21 @@ var passport = require('passport')
 var csrfProtection = csrf()
 router.use(csrfProtection)
 
+//Get user profile page
+router.get('/profile', isLoggedIn, function(req, res, next) {
+  res.render('user/profile')
+})
+
+//退出登录
+router.get('/logout', isLoggedIn, function(req, res, next) {
+  req.logout()
+  res.redirect('/')
+})
+
+//检查是否登录
+router.use('/',notLoggedIn,function (req,res,next) {
+  next()
+})
 
 //Get user signup page
 router.get('/signup', function(req, res, next) {
@@ -19,29 +34,42 @@ router.get('/signup', function(req, res, next) {
 
 //Post user signup
 router.post('/signup', passport.authenticate('local.signup', {
-        successRedirect: '/user/profile',
-        failureRedirect: '/user/signup',
-        failureFlash: true
+    successRedirect: '/user/profile',
+    failureRedirect: '/user/signup',
+    failureFlash: true
 }))
 
 //Get user signin
-router.get('/signin',function (req,res,next) {
-  var messages = req.flash('error')
-  res.render('user/signin', {
-      csrfToken: req.csrfToken(),
-      messages: messages,
-      hasErrors: messages.length > 0
-  })
+router.get('/signin', function(req, res, next) {
+    var messages = req.flash('error')
+    res.render('user/signin', {
+        csrfToken: req.csrfToken(),
+        messages: messages,
+        hasErrors: messages.length > 0
+    })
 })
 
 //Post user signin
-router.post('/signin',passport.authenticate('local.signin',{
-  successRedirect: '/user/profile',
-  failureRedirect: '/user/signin',
-  failureFlash: true
-}))
-//Get user profile page
-router.get('/profile', function(req, res, next) {
-    res.render('user/profile')
-})
+router.post('/signin', passport.authenticate('local.signin', {
+        successRedirect: '/user/profile',
+        failureRedirect: '/user/signin',
+        failureFlash: true
+    }))
+
+
 module.exports = router
+
+//已经登录
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/')
+}
+//未登录
+function notLoggedIn(req, res, next) {
+    if (!req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect('/')
+}
